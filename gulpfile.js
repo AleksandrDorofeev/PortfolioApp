c4b174cc31dff62f5a1e659199ede81db56e9ea4
Aleksandr <alzumar91@mail.ru>
2017-08-27 16:03:55 +0300

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
-let gulp = require('gulp');
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
+
+gulp.task('sass', () => {
+  return gulp.src('src/scss/style.scss')
+        .pipe (sass())
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: "public"
+    },
+    notify: false
+  })
+});
+
+gulp.task('watch',['serve', 'sass'], function() {
+  gulp.watch('src/scss/style.scss', ['sass'])
+  gulp.watch('public/*.html', browserSync.reload)
+  gulp.watch('public/index.html', browserSync.reload)
+});
+
+gulp.task('default', ['watch']);
