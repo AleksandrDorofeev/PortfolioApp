942063ce779ebd6e6ea0f8376c0212e3b0fbf943
Aleksandr <alzumar91@mail.ru>
2017-09-09 13:03:13 +0300

    dropdown

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,4 @@ $(document).ready(function() {
   $(".header button").click(function() {
     $(".main-nav").slideToggle(300);
   });
-
-  //$(window).resize(function() {
-  if (screen.width > 1024) {
-    $('.main-nav').show();
-  } else {
-    $('.main-nav').hide();
-    }
-  //});
 });
