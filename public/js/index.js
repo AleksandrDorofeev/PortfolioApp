af7e6d77bf8fee5e5359975d7989e65c87cd0896
Aleksandr <alzumar91@mail.ru>
2017-09-11 01:50:33 +0300

    rework html scrollup

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+
+  /* Dropdown */
   $(".header button").click(function() {
     $(".main-nav").slideToggle(300);
+  });
+
+  /*Scroll Up */
+  $(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+    $('.scrollup').fadeIn();
+  } else {
+    $('.scrollup').fadeOut();
+  }
+  });
+
+  $('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
   });
 });
